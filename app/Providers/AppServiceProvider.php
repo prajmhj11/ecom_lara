fa346aa7e9c88e4b04db252bf5972b3ed44ae364
prajmhj11 <praj.mhj11@gmail.com>
2020-11-03 14:50:00 +0545

    fix pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\AbstractPaginator;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
@@ -26,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        $paginator = new AbstractPaginator();
+        $paginator->onEachSide(1);
         Paginator::useBootstrap();
     }
 }
