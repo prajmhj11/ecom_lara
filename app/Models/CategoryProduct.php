0c9d11188288abab8bbe67d37a1811e1d50d175b
prajmhj11 <praj.mhj11@gmail.com>
2020-11-04 09:02:42 +0545

    create CategoryProduct model

--- app/Models/CategoryProduct.php
+++ app/Models/CategoryProduct.php
@@ -1,13 +1,12 @@
 <?php
 
-namespace BlogApp\SharedModels;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Order extends Model
+class CategoryProduct extends Model
 {
     use HasFactory;
-
-
+    protected $table = 'category_product';
 }
--- resources/views/layouts/ecom/product.blade.php
+++ resources/views/layouts/ecom/product.blade.php
@@ -22,13 +22,13 @@
     @include('layouts.ecom.partials.alert')
         <div class="row">
             <div class="col-md-4 product-section-image d-flex align-items-center justify-content-center">
-                <img src="{{asset('img/products/'.$product->slug.'.jpg')}}" alt="product">
+                <img src="{{asset('img/products/'.$product->slug.'.jpg') }}" alt="product">
             </div>
             <div class="col-md-8 product-section-information">
                 <h1 class="product-section-title">{{$product->name}}</h1>
                 <div class="product-section-subtitle cursor-default">{{ $product->details }}</div>
                 <div class="product-section-price cursor-default">{{ $product->presentPrice() }}</div>
-                <div class="product-section-description"><p>{{ $product->description }}</p></div>
+                <div class="product-section-description"><p>{!! $product->description !!}</p></div>
                 <!-- <a href="#" class="button">Add to Cart</a> -->
                 <form action="{{route('cart.store')}}" method="post">
                     @csrf
